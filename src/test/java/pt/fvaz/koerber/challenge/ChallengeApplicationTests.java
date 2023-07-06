61f5e3a06afad73682408cebe0b85618720c6665
Felicio Vaz <felicio.vaz@nokia.com>
2023-07-06 05:57:08 +0100

    first test with spring context

---
+++ src/test/java/pt/fvaz/koerber/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.fvaz.koerber.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
